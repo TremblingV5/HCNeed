b23fbc047a8d2a5fc51d41ab87561cd04be64623
xinzf <zf.xin@qq.com>
2024-01-26 00:31:50 +0800

    update

--- server/src/main/java/org/hcneed/server/common/crawler/BaseCrawler.java
+++ server/src/main/java/org/hcneed/server/common/crawler/BaseCrawler.java
@@ -1,4 +1,17 @@
 package org.hcneed.server.common.crawler;
 
-public class BaseCrawler {
+import java.net.HttpURLConnection;
+import java.util.Map;
+
+public abstract class BaseCrawler<T> {
+    public Map<String, String> headers;
+    public String url;
+
+    public BaseCrawler() {
+        headers.put("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36");
+    }
+
+    public void job() {
+
+    }
 }
